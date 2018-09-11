743a8a65afe0e0abf889600537574f068e809968
stilianoudakis <stilianoudasc@mymail.vcu.edu>
2018-09-11 01:38:51 -0400

    initial commit

---
+++ GM12878/Counts_vs_Widths/countsvwidths.R
@@ -0,0 +1,231 @@
+# Loading Libraries
+
+
+
+library(GenomicRanges)
+library(caret)
+library(data.table)
+library(gbm)
+library(randomForest)
+library(glmnet)
+library(pROC)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(DMwR)
+library(gridExtra)
+library(pROC)
+library(ROCR)
+library(leaps)
+
+
+
+# Reading in data
+
+setwd("/home/stilianoudakisc/TAD_data_analysis/model_filtering/count_vs_width/")
+
+gm12878_5kb <- readRDS("gm12878_5kb.rds")
+
+gm12878_5kb <- gm12878_5kb[,grep("dist", names(gm12878_5kb), invert=TRUE)]
+
+##Changing levels of response (y) to yes no
+gm12878_5kb$y <- factor(gm12878_5kb$y)
+
+levels(gm12878_5kb$y) <- c("No", "Yes")
+
+### set number of bootstrap samples
+
+bootsamps = 10
+
+### set tuning parameters
+
+fitControl <- trainControl(method = "cv",
+                           number = 10,
+                           ## Estimate class probabilities
+                           classProbs = TRUE,
+                           ## Evaluate performance using 
+                           ## the following function
+                           summaryFunction = twoClassSummary)
+						   
+#### function for roc curves
+
+simple_roc <- function(labels, scores){
+  labels <- labels[order(scores, decreasing=TRUE)]
+  data.frame(TPR=cumsum(labels)/sum(labels), FPR=cumsum(!labels)/sum(!labels), labels)
+}
+						
+###create a matrix of row ids that represent the zero class
+
+##the number of rows will match the one class
+##the number of columns match the number of bootstrap samples
+
+sampids <- matrix(ncol=bootsamps, 
+                  nrow=length(gm12878_5kb$y[which(gm12878_5kb$y=="Yes")]))
+
+
+###filling in the sample ids matrix
+
+set.seed(123)
+for(j in 1:bootsamps){
+  sampids[,j] <- sample(which(gm12878_5kb$y=="No"),
+                        length(which(gm12878_5kb$y=="Yes")),
+                        replace = TRUE)
+}
+
+		
+# Final Models:
+
+# Elastic Net
+
+#set length of list objects that will be filled in with specificities
+#and sensitivities and aucs and variable importance
+rflst <- list(tpr <- matrix(nrow=ceiling((length(which(gm12878_5kb$y=="Yes"))*2)*.3), 
+                                  ncol=bootsamps),
+                    fpr <- matrix(nrow=ceiling((length(which(gm12878_5kb$y=="Yes"))*2)*.3), 
+                                  ncol=bootsamps),
+                    varimp <- matrix(nrow=dim(gm12878_5kb)[2]-1,
+                                     ncol=bootsamps) #,
+					#coefs <- matrix(nrow=dim(gm12878_5kb)[2]-1,
+					#				ncol=bootsamps)
+					)
+rownames(rflst[[3]]) <- colnames(gm12878_5kb)[-1]
+#rownames(rflst[[4]]) <- colnames(gm12878_5kb)[-1]
+
+rfperf <- matrix(nrow = 17, ncol=bootsamps)
+rownames(rfperf) <- c("TN",
+                            "FN",
+                            "FP",
+                            "TP",
+                            "Total",
+                            "Sensitivity",
+                            "Specificity",
+                            "Kappa",
+                            "Accuracy",
+                            "Precision",
+                            "FPR",
+                            "FNR",
+                            "FOR",
+                            "NPV",
+                            "MCC",
+                            "F1",
+                            "AUC")
+
+for(i in 1:bootsamps){
+  set.seed(7215)
+  #combining the two classes to create balanced data
+  data <- rbind.data.frame(gm12878_5kb[which(gm12878_5kb$y=="Yes"),],
+                           gm12878_5kb[sampids[,i],])
+  
+  
+  #determining the best number of variables randomly sampled as candidates at each split
+  set.seed(5430)
+  bestmtry <- tuneRF(data[,-1],data$y,
+                   improve=.01,trace=0,plot=F) 
+  bestmtry <- data.frame(bestmtry)
+  bestmtry <- bestmtry[order(bestmtry$OOBError, decreasing = FALSE),]
+  #bestmtry$mtry[1]
+
+  #splitting the data
+  inTrainingSet <- sample(length(data$y),floor(length(data$y)*.7))
+  #inTrainingSet <- createDataPartition(data$y,p=.7,list=FALSE)
+  train <- data[inTrainingSet,]
+  test <- data[-inTrainingSet,]
+  
+  #determining best number of trees
+  tunegrid <- expand.grid(.mtry=bestmtry$mtry[1])
+  modellist <- list()
+    for (ntree in c(50,200,500,1000)) {
+      set.seed(333)
+      fit <- train(y~., data=train, 
+                   method="rf", 
+                   metric="Accuracy",
+                   tuneGrid=tunegrid,  
+                   ntree=ntree)
+      key <- toString(ntree)
+      modellist[[key]] <- fit
+    }
+    # compare results
+    results <- resamples(modellist)
+    #summary(results)
+    #dotplot(results)
+    results <- data.frame(summary(results)[3]$statistics$Accuracy)
+    results <- results[order(results$Mean, decreasing = TRUE),]
+
+  set.seed(1006)
+  rfModel <- train(y~., data=train, 
+                    method="rf", 
+                    metric="ROC", 
+                    tuneGrid=tunegrid, 
+                    trControl=fitControl, 
+                    ntree=as.numeric(rownames(results)[1]))
+
+  #Prediction vector for ROC and AUC
+  pred.rfModel <- as.vector(predict(rfModel, 
+                                    newdata=test, 
+                                    type="prob")[,"Yes"])
+  rflst[[1]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.rfModel)[,1]
+  rflst[[2]][,i] <- simple_roc(ifelse(test$y=="Yes",1,0),pred.rfModel)[,2]
+  rflst[[3]][,i] <- varImp(rfModel)$importance[,1]
+  
+  #Prediction vector for other performance metrics
+  pred.rfModel2 <- predict(rfModel,
+                                     newdata=test,
+                                     type="raw")
+  confMat <- confusionMatrix(data=pred.rfModel2, test$y, positive="Yes")
+  TN = as.numeric(confMat$table[1,1])
+  FN = as.numeric(confMat$table[1,2])
+  FP = as.numeric(confMat$table[2,1])
+  TP = as.numeric(confMat$table[2,2])
+  rfperf[1,i] <- TN
+  rfperf[2,i] <- FN
+  rfperf[3,i] <- FP
+  rfperf[4,i] <- TP
+  rfperf[5,i] <- sum(confMat$table)
+  rfperf[6,i] <- as.vector(confMat$byClass["Sensitivity"])
+  rfperf[7,i] <- as.vector(confMat$byClass["Specificity"])
+  rfperf[8,i] <- as.vector(confMat$overall["Kappa"])
+  rfperf[9,i] <- as.vector(confMat$overall["Accuracy"])
+  rfperf[10,i] <- TP/(TP+FP)
+  rfperf[11,i] <- FP/(FP+TN)
+  rfperf[12,i] <- FN/(FN+TN)
+  rfperf[13,i] <- FN/(FN+TN)
+  rfperf[14,i] <- TN/(TN+FN)
+  rfperf[15,i] <- (TP*TN - FP*FN)/( sqrt( (TP+FP)*(TP+FN)*(TN+FP)*(TN+FN) ) )
+  rfperf[16,i] <- (2*(TP/(TP+FN))*(TP/(TP+FP)))/((TP/(TP+FN)) + ((TP/(TP+FP))))
+  rfperf[17,i] <- pROC::auc(pROC::roc(test$y, pred.rfModel))
+  
+}
+
+saveRDS(rflst,"rflst_cvw.rds")
+saveRDS(rfperf,"rfperf_cvw.rds")	
+
+####################################################################################
+
+rflst_cvw <- readRDS("rfperf_cvw.rds")
+
+varimp.cvw <- rowMeans(rflst_cvw[[3]])
+varimp.cvw.df <- data.frame(Feature = rownames(rflst_cvw[[3]]), Importance =as.vector( varimp.cvw))
+varimp.cvw.df <- varimp.cvw.df[order(varimp.cvw.df$Importance, decreasing=FALSE),]
+varimp.cvw.df <- varimp.cvw.df[(dim(varimp.cvw.df)[1]-9):dim(varimp.cvw.df)[1],]
+varimp.cvw.df$Feature <- factor(varimp.cvw.df$Feature,
+                                     levels=varimp.cvw.df$Feature)
+p.cvw <- ggplot(varimp.cvw.df, aes(x=Feature, y=Importance)) +
+  xlab("Predictors") +
+  ylab("Importance") +
+  #ggtitle("Using Both Counts and Widths of Overlaps") +
+  geom_bar(stat="identity", 
+           width=.5, 
+           position="dodge",
+           fill=rainbow(3)[3]) +
+  coord_flip() +
+  theme_minimal() +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold")) +
+		#axis.text.x = element_text(angle = 90, hjust = 1),
+		#legend.title=element_text(size=20), 
+		#legend.text=element_text(size=15),
+		#legend.position = c(.7,.9)
+  ggtitle("Elastic-Net")
+p.cvw	
+ggsave("/home/stilianoudakisc/TAD_data_analysis/model_filtering/count_vs_width/p.cvw.png")
+
---
+++ GM12878/Counts_vs_Widths/histone.R
@@ -0,0 +1,197 @@
+#Model filtering
+
+#Loading Libraries
+
+library(GenomicRanges)
+library(caret)
+library(data.table)
+library(gbm)
+library(randomForest)
+library(glmnet)
+library(pROC)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(DMwR)
+library(gridExtra)
+library(pROC)
+library(ROCR)
+library(leaps)
+
+# Creating Response Vector Y
+
+## Reading in TAD data
+
+domains <- read.table("/home/stilianoudakisc/TAD_data_analysis/GM12878/arrowhead_data.txt", header=T)
+domains <- domains[,1:3]
+head(domains)
+dim(domains) #9274    3
+
+#removing chrX from boundary data
+domains <- domains[-which(domains$Chromosome=="chrX"),]
+
+#creating granges object out of tad boundary data
+
+#concatenating boundary coordinates into one long vector
+coords <- domains
+colnames(coords)[2:3] <- c("coordinate", "coordinate")
+coords <- rbind.data.frame(coords[,c(1,2)],coords[,c(1,3)])
+coords$Chromosome <- as.character(coords$Chromosome)
+
+#sorting by chromosome and coordinate
+coords <- coords[order(as.numeric(substr(coords$Chromosome,4,5)), coords$coordinate, decreasing = FALSE),]
+
+#remove duplicates for coordinates that are conjoined
+coords <- coords[!duplicated(coords),]
+dim(coords) #16770     2
+
+coords$lagchrom <- lag(coords$Chromosome)
+coords$lagchrom[which(coords$Chromosome!=coords$lagchrom)] <- NA
+coords$lagcoord <- lag(coords$coordinate)
+coords$lagcoord[which(is.na(coords$lagchrom))] <- NA
+
+bounds <- GRanges(seqnames=coords$Chromosome, ranges=IRanges(start=coords$coordinate, width=1))
+bounds_f <- flank(bounds, width=5000, both=TRUE)
+
+#saving data
+#saveRDS(bounds, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/bounds.rds")
+#saveRDS(bounds_f, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/bounds_f.rds")
+
+
+## Reading in binned genome in the form of contact matrix at 10kb resolution
+
+binslist5 <- read.table("/home/stilianoudakisc/TAD_data_analysis/GSE63525_data/contact_matrices/binned_data/5kb/binslist5.txt")
+
+dim(binslist5) #536396      2
+
+#ordering the bins according to left endpoint
+binslist5 <- binslist5[order(as.numeric(substr(binslist5$V1,4,5)), binslist5$V2, decreasing=FALSE),]
+
+#extracting and renaming first 2 columns
+binslist5 <- binslist5[,1:2]
+colnames(binslist5) <- c("Chromosome", "Coordinate")
+
+
+#creating a granges object from binned genome
+binslist5 <- GRanges(seqnames = binslist5$Chromosome, ranges = IRanges(start = binslist5$Coordinate,
+                                                                         width = 5000))
+																		 
+
+#saving the binned genome data
+#saveRDS(binslist5, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/binslist5.rds")
+
+
+## Creating Response Vector Y (1 if tad boundary is in bin; 0 if not)
+
+#Finding where the TAD boundaries are inside the genomic bins
+findOverlaps(binslist5, bounds_f)
+
+y <- countOverlaps(binslist5, bounds_f)
+length(y) #536396
+table(y)
+#     0      1      2
+#504576  30126   1694
+
+
+
+#investigating where there are multiple overlaps
+head(which(y==2))  #324 1456 1479 1516 1604 1698
+
+#looking at row 169 of binned genome
+findOverlaps(binslist5[324], bounds_f)
+#      queryHits subjectHits
+#      <integer>   <integer>
+#  [1]         1           9
+#  [2]         1          10
+
+bounds_f[9:10]
+#      seqnames             ranges strand
+#         <Rle>          <IRanges>  <Rle>
+#  [1]     chr1 [1855000, 1864999]      *
+#  [2]     chr1 [1860000, 1869999]      *
+
+
+#recode multiple overlaps as 1
+y <- ifelse(y>=1,1,0)
+prop.table(table(y))  
+#         0          1
+#0.94067816 0.05932184
+
+mcols(binslist5)$y <- y
+
+###########################################################################################
+
+binslist5 <- binslist5[seqnames(binslist5)=="chr22"]
+y <- countOverlaps(binslist5, bounds_f)
+y <- ifelse(y>=1,1,0)
+mcols(binslist5)$y <- y
+
+
+## Creating the data frame with resonse vector for modeling
+
+gm12878_5kb <- data.frame(y = y)
+
+
+#saveRDS(gm12878_5kb, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/gm12878_5kb.rds")
+
+#creating a granges object from the center of every range in the binned genome granges object
+#this will be used to calculate distances from genomic feature to center of genomic bin
+binslist5_center <- resize(binslist5, width = 1, fix = "center")
+
+#saveRDS(binslist5_center, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/binslist5_center.rds")
+
+
+# Creating Feature Space
+
+
+## Histone Modifications
+
+setwd("/home/stilianoudakisc/TAD_data_analysis/annotations/HistoneModifications/")
+
+temp <- list.files()
+temp <- temp[grep("Gm12878",temp)]
+
+for(i in 1:length(temp)){
+	dat <- read.table(temp[i],header = FALSE, sep="\t")
+	dat_gr <- GRanges(seqnames=dat$V1,
+						IRanges(start=dat$V2, 
+								end=dat$V3))
+	dat_gr_center <- resize(dat_gr, width = 1, fix = "center")
+	
+	w <- unlist(lapply(binslist5, function(x){
+							sum(width(
+									ranges(
+										pintersect(
+											findOverlapPairs(x,dat_gr)))))}))
+
+	gm12878_5kb <- cbind.data.frame(gm12878_5kb,w)
+	names(gm12878_5kb)[length(names(gm12878_5kb))] <- sub(".bed", "", temp[i])
+	
+	d <- mcols(distanceToNearest(binslist5_center, dat_gr_center))$distance
+	
+	gm12878_5kb <- cbind.data.frame(gm12878_5kb,d)
+	names(gm12878_5kb)[length(names(gm12878_5kb))] <- paste(sub(".bed", "", temp[i]), "_dist", sep="")
+
+	c <- countOverlaps(binslist5, dat_gr)
+	
+	gm12878_5kb <- cbind.data.frame(gm12878_5kb,c)
+	names(gm12878_5kb)[length(names(gm12878_5kb))] <- paste(sub(".bed", "", temp[i]), "_count", sep="")
+	
+	}
+
+
+
+## Saving the data
+
+dim(gm12878_5kb)
+
+head(gm12878_5kb)
+
+histone <- gm12878_5kb
+
+saveRDS(histone, "/home/stilianoudakisc/TAD_data_analysis/model_filtering/count_vs_width/histone.rds")
+
+
+
+
+
